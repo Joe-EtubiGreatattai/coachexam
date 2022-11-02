2127c0d91b1b937bd6b757243370c42d1eb4e733
Joe-EtubiGreatattai <98915249+Joe-EtubiGreatattai@users.noreply.github.com>
2022-11-02 16:41:55 +0100

    Add files via upload

---
+++ JS/webdev.js
@@ -0,0 +1,120 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Which program is used by web clients to view the web pages ?",
+    answer: "Web browser",
+    options: [
+      "Web browser",
+      "Protocol",
+      "Web server",
+      "Search Engine"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which tag is used to display text in title bar of a web document ?",
+    answer: "Title tags",
+    options: [
+      "Body tag",
+      "Meta tag",
+      "Title tags",
+      "Comment tag"
+    ]
+  },
+    {
+    numb: 3,
+    question: "The language that instructs the browser on how to display the hypertext, and adds pictures to the document is __",
+    answer: "HTML",
+    options: [
+      "C",
+      "COBOL",
+      "python",
+      "HTML"
+    ]
+  },
+    {
+    numb: 4,
+    question: "HTML allows us to use ___ levels of headings",
+    answer: "6",
+    options: [
+      "2",
+      "4",
+      "6",
+      "5"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which character is used after the opening angular bracket to represent the closing tag?",
+    answer: "/",
+    options: [
+      "?",
+      "/",
+      "-",
+      "("
+    ]
+  },
+
+  {
+    numb: 6,
+    question: "____ are ignored by browser.",
+    answer: "Comments",
+    options: [
+      "Title tag",
+      "Comments",
+      "Body section",
+      "Head section"
+    ]
+  },
+ 
+  
+  {
+    numb: 7,
+    question: "Which term is used to refer to the computers that are used for storing web pages as files?",
+    answer: "Web servers",
+    options: [
+      "Internet",
+      "Web clients",
+      "Web servers",
+      "World Wide Web"
+    ]
+  },
+ 
+  {
+    numb: 8,
+    question: "What is the abbreviation of HTML?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Tag Markup Language",
+      "Hyper Text Markup Language",
+      "Hyper Text Main Language",
+      "Hyper Tag Main Language"
+    ]
+  },
+
+  {
+    numb: 9,
+    question: "The full form of CSS is:",
+    answer: "Cascading Style Sheets",
+    options: [
+      "Coloring and Style Sheets",
+      "Cascading Style Sheets",
+      "Color and Style Sheets",
+      "None of the above"
+    ]
+  },
+
+  {
+    numb: 10,
+    question: "How can we change the background color of an element?",
+    answer: "background-color",
+    options: [
+      "background-color",
+      "color",
+      "Both A and B",
+      "None of the above"
+    ]
+  },
+];
+
